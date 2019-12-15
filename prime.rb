8fbfe11d71031979c23062b0b3d0052d32025f59
Robert M Ricci <rmricci121@gmail.com>
2019-12-15 12:09:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,19 +4,8 @@ range = (2..(number - 1)).to_a
 if number > 1 
   range.none? do |n|
     number % n == 0 
-  end
+    end
   else
     false
-
-
-
-
-
-
-# if number % num == 0 
-#   false 
-#     end
-#   end
-#   true
-end
+  end
 end
